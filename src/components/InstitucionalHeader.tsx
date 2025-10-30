831b065aa200b496aa89f958862b6d3083cca7e1
El-Nuez <guitarra.cesar@hotmail.com>
2025-10-30 01:09:40 -0600

    Actualizar la ruta de la imagen del logo institucional en el componente InstitucionalHeader

--- src/components/InstitucionalHeader.tsx
+++ src/components/InstitucionalHeader.tsx
@@ -12,7 +12,7 @@ const InstitucionalHeader = ({
       <div className="flex items-center gap-5">
         {/* Aquí se coloca el logo institucional */}
         <img
-          src="src\logo.jpeg"
+          src="public\logo.jpeg"
           alt="Logo institucional"
           className="h-12 w-auto object-contain"
         />
